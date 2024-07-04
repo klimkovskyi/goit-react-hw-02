c92f33b1a7fc37530a585818f484cc721e9863e8
Andrew <36772492+klimkovskyi@users.noreply.github.com>
2024-07-04 17:29:33 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import userData from "./userData.json";
+import friends from "./friends.json";
+import transactions from "./transactions.json";
+import Profile from "./components/Profile/Profile";
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+
+function App() {
+  const { username, tag, location, avatar, stats } = userData;
+
+  return (
+    <>
+      <Profile
+        name={username}
+        tag={tag}
+        location={location}
+        image={avatar}
+        stats={stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,15 @@
+import css from "./FriendListItem.module.css";
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <div>
+      <img src={avatar} alt="Avatar" width="48" />
+      <p>{name}</p>
+      <p className={isOnline ? css.isOnline : css.offline}>
+        {isOnline ? "Online" : "Offline"}
+      </p>
+    </div>
+  );
+};
+
+export default FriendListItem;
